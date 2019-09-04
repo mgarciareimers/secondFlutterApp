8bcd62c0246c13541ade9ab6e3b51ed6c7b02cee
miguelgarciareimers <miguel.garcia@magocode.com>
2019-09-04 18:41:22 +0200

    Alert + Avatar Page developed

--- components_app/lib/src/pages/alert_page.dart
+++ components_app/lib/src/pages/alert_page.dart
@@ -8,12 +8,57 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Show alert'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () {
+            this._showAlert(context);
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Content of the alert'),
+              FlutterLogo(
+                size: 50,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        );
+      }
     );
   }
 
--- components_app/lib/src/pages/avatar_page.dart
+++ components_app/lib/src/pages/avatar_page.dart
@@ -7,6 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY5SFzGGICx5-AMRYhVx8Ggu89NecZ9W4dAHQhOLpMc0TtvVp0'),
+              radius: 22,
+            ),
+          ),
+          Container(
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.green,
+            ),
+            margin: EdgeInsets.only(right: 10),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://staticuestudio.blob.core.windows.net/buhomag/2016/03/22154524/stan.jpg?auto=compress,format'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
